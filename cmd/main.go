fdbf2da43f84f453770bb8752cc7331c7a53c236
Arshia Dadras <arshiadadras1234@gmail.com>
2024-08-06 17:37:17 +0330

    saving gps points next to other files to be readble in benchmarks

--- cmd/main.go
+++ cmd/main.go
@@ -29,22 +29,18 @@ func main() {
 	}
 	log.Println("Map matching completed successfully")
 
+	if err := internal.SaveObject(points, "data/points.json"); err != nil {
+		log.Fatalf("Error writing points: %v", err)
+	}
+	log.Println("Points written successfully")
 	if err := internal.SaveObject(edges, "data/edges.json"); err != nil {
 		log.Fatalf("Error writing edges: %v", err)
 	}
 	log.Println("Edges written successfully")
-
 	if err := internal.SaveObject(graph, "data/graph.json"); err != nil {
 		log.Fatalf("Error writing graph: %v", err)
 	}
 	log.Println("Graph written successfully")
 
 	log.Println("All tasks completed successfully")
-
-	// P1 := pkg.Point{Longitude: 51.372723607382994, Latitude: 35.792019008390966}
-	// P2 := pkg.Point{Longitude: 51.371323391458816, Latitude: 35.79170002313525}
-	// P3 := pkg.Point{Longitude: 51.37272618815106, Latitude: 35.79319153141927}
-
-	// fmt.Println("Distance between P1 and P2:", P1.Distance(P2), "meters")
-	// fmt.Println("Distance between P1 and P3:", P1.Distance(P3), "meters")
 }
