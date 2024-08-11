09b93b5a430cd42432fd1faff6c65c9ab76748b6
Arshia Dadras <arshiadadras1234@gmail.com>
2024-08-11 14:21:01 +0330

    adding some new useful functions and summarizing some parts

--- pkg/point.go
+++ pkg/point.go
@@ -14,15 +14,19 @@ type Point struct {
 	Latitude  float64 `json:"latitude"`
 }
 
+func toRadians(deg float64) float64 {
+	return deg * math.Pi / 180
+}
+
+func toDegrees(rad float64) float64 {
+	return rad * 180 / math.Pi
+}
+
 func (p *Point) Distance(other Point) float64 {
-	lat1 := p.Latitude * math.Pi / 180
-	long1 := p.Longitude * math.Pi / 180
-	lat2 := other.Latitude * math.Pi / 180
-	long2 := other.Longitude * math.Pi / 180
+	lat1, long1 := toRadians(p.Latitude), toRadians(p.Longitude)
+	lat2, long2 := toRadians(other.Latitude), toRadians(other.Longitude)
 
-	dlat := lat2 - lat1
-	dlong := long2 - long1
-
+	dlat, dlong := lat2-lat1, long2-long1
 	a := math.Pow(math.Sin(dlat/2), 2) + math.Cos(lat1)*math.Cos(lat2)*math.Pow(math.Sin(dlong/2), 2)
 	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
 
@@ -31,25 +35,38 @@ func (p *Point) Distance(other Point) float64 {
 
 func (p *Point) Move(dx, dy float64) Point {
 	return Point{
-		Longitude: p.Longitude + (180 * dx / (math.Pi * EarthRadius * math.Cos(p.Latitude*math.Pi/180))),
-		Latitude:  p.Latitude + (180 * dy / (math.Pi * EarthRadius)),
+		Longitude: p.Longitude + toDegrees(dx/(EarthRadius*math.Cos(toRadians(p.Latitude)))),
+		Latitude:  p.Latitude + toDegrees(dy/EarthRadius),
 	}
 }
 
 func (p *Point) ClosestPointOnSegment(a, b Point) Point {
-	ap := Point{Longitude: p.Longitude - a.Longitude, Latitude: p.Latitude - a.Latitude}
-	ab := Point{Longitude: b.Longitude - a.Longitude, Latitude: b.Latitude - a.Latitude}
-	t := (ap.Longitude*ab.Longitude + ap.Latitude*ab.Latitude) / (ab.Longitude*ab.Longitude + ab.Latitude*ab.Latitude)
+	latP, longP := toRadians(p.Latitude), toRadians(p.Longitude)
+	latA, longA := toRadians(a.Latitude), toRadians(a.Longitude)
+	latB, longB := toRadians(b.Latitude), toRadians(b.Longitude)
 
-	if t < 0 {
+	xP, yP, zP := math.Cos(latP)*math.Cos(longP), math.Cos(latP)*math.Sin(longP), math.Sin(latP)
+	xA, yA, zA := math.Cos(latA)*math.Cos(longA), math.Cos(latA)*math.Sin(longA), math.Sin(latA)
+	xB, yB, zB := math.Cos(latB)*math.Cos(longB), math.Cos(latB)*math.Sin(longB), math.Sin(latB)
+
+	ABx, ABy, ABz := xB-xA, yB-yA, zB-zA
+	APx, APy, APz := xP-xA, yP-yA, zP-zA
+
+	abAb := ABx*ABx + ABy*ABy + ABz*ABz
+	apAb := APx*ABx + APy*ABy + APz*ABz
+	projFactor := apAb / abAb
+	if projFactor < 0 {
 		return a
-	} else if t > 1 {
+	} else if projFactor > 1 {
 		return b
-	} else {
-		return Point{
-			Longitude: a.Longitude + ab.Longitude*t,
-			Latitude:  a.Latitude + ab.Latitude*t,
-		}
+	}
+
+	x, y, z := xA+projFactor*ABx, yA+projFactor*ABy, zA+projFactor*ABz
+	lat, long := math.Asin(z), math.Atan2(y, x)
+
+	return Point{
+		Latitude:  toDegrees(lat),
+		Longitude: toDegrees(long),
 	}
 }
 
@@ -73,4 +90,17 @@ func (p *Point) ClosestPointOnEdge(edge *Edge) (closestPoint Point) {
 
 func (p *Point) DistanceToEdge(edge *Edge) float64 {
 	return p.Distance(p.ClosestPointOnEdge(edge))
+}
+
+func (a *Point) MoveTowards(b Point, d float64) Point {
+	latA, longA := toRadians(a.Latitude), toRadians(a.Longitude)
+	latB, longB := toRadians(b.Latitude), toRadians(b.Longitude)
+
+	latDiff, longDiff := latB-latA, longB-longA
+	lat, long := latA+latDiff*d/a.Distance(b), longA+longDiff*d/a.Distance(b)
+
+	return Point{
+		Latitude:  toDegrees(lat),
+		Longitude: toDegrees(long),
+	}
 }
